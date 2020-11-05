7138aa1d95607daf259d7a59d75a1238ed490748
ldaijiw <lwaltmann@spartaglobal>
2020-11-05 15:30:11 +0000

    init order to check which menu they'd like to order from

--- restaurant.py
+++ restaurant.py
@@ -4,23 +4,35 @@ class Menu(dict):
             self.update({food: price})
     
     def __str__(self):
-        print("Welcome!\nWe have amazing things on our menu:")
+        print("\n\n***\nWelcome!\nWe have amazing things on our menu:")
 
         # prints menu items in nice format with price formatting and capitalisation
         for food, price in self.items():
             print("{}, Price: £{:,.2f}".format(food.title(), price))
 
-        return "Hope you enjoy your visit"
+        return "Hope you enjoy your visit\n***"
 
-class Order()
-
+class Order(list):
+    def __init__(self):
+        allowed_menus = [k for k, v in globals().items() if isinstance(v, Menu)]
+        
+        print("\n***Possible Menus:\n")
+        for menu in allowed_menus:
+            print(menu.replace('_', ' ').title())
+        
+        self.menu = input("\nWhich menu will you be ordering from?\n").lower().replace(' ', '_')
+        assert self.menu in allowed_menus
+        print(self.menu)
+        
+        
 
 if __name__ == "__main__":
     japanese_items = {"sushi": 9, "ramen": 5, "katsu curry": 6}
-
+    lunch_items = {"katsu curry": 5, "onigiri": 2}
+    
     japan_menu = Menu(japanese_items)
-    print(japan_menu)
-    print(japan_menu.keys())
-    print(japan_menu.values())
+    japan_lunch_menu = Menu(lunch_items)
+    print(japan_lunch_menu)
     japan_menu["ramune"] = 3
-    print(japan_menu)
+    
+    leo_order = Order()
